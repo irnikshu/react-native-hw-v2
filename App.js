45dbfb44ecbb43ff529fc8403e322489cd0cb3f7
irnikshu <95768966+irnikshu@users.noreply.github.com>
2023-03-30 10:00:02 +0200

    ?

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* <ImageBackground 
+        style={styles.image}
+        source={require("./assets/images/auth-bcg-image.jpg")}>        
+        </ImageBackground> */}
       <View style={styles.innerBox}>
         <Text style={styles.text}>Регистрация</Text>
       </View>
@@ -27,5 +31,10 @@ const styles = StyleSheet.create({
   innerBox: {
     width: 184,
     height: 35,
+  },
+  image: {
+     flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
   }
 });
